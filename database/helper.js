d93c220b5ff5fc496cb6a7bc9c065ffc7b7ec747
Zulkarnain <prof.bub@live.com>
2020-04-13 01:00:23 +0700

    refactor(database): add migration

---
+++ database/helper.js
@@ -0,0 +1,7 @@
+module.exports = {
+  addTriggerUpdatedAt: (queryInterface, tableName, options = {}) => queryInterface.sequelize.query(`
+    DROP TRIGGER IF EXISTS "set_updated_at" ON "${ tableName }";
+    CREATE TRIGGER "set_updated_at" BEFORE UPDATE ON "${ tableName }"
+    FOR EACH ROW EXECUTE PROCEDURE trigger_updated_at_column();
+  `, options)
+};
