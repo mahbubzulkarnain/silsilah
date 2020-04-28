8247c3fbcc3db8b108e11741eeeef0651c7143b0
Zulkarnain <prof.bub@live.com>
2020-04-29 01:50:34 +0700

    refactor(backend): change tslint to eslint airbnb

--- backend/jest.setup.js
+++ backend/jest.setup.js
@@ -1,4 +1,8 @@
-require("dotenv")?.config();
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dotenv = require('dotenv');
+
+// eslint-disable-next-line no-unused-expressions
+dotenv && dotenv.config();
 
 process.env.NODE_ENV = 'DEV';
 
